fc72fe4e0b02c72d19ab955a92fd67f2390d74ad
Sergio Govoni <segovoni@gmail.com>
2021-11-01 17:20:25 +0100

    Squashed 'LibrariesSQL/' content from commit 37ac3d9
    git-subtree-dir: LibrariesSQL
    git-subtree-split: 37ac3d9a804e5fbac0caff4f12f53a42927a50e5

---
+++ unit-test/sp-alter-column-unit-test-25-setup-system-under-test.sql
@@ -0,0 +1,24 @@
+------------------------------------------------------------------------
+-- Project:      sp_alter_column                                       -
+--               https://github.com/segovoni/sp_alter_column           -
+--               The stored procedure is able to alter a column        -
+--               with dependencies in your SQL database. It composes   -
+--               automatically the appropriate DROP and CREATE         -
+--               commands for each object connected to the column      -
+--               I want to modify                                      -
+--                                                                     -
+-- File:         Unit test - Setup SUT (System Under Test)             -
+-- Author:       Sergio Govoni https://www.linkedin.com/in/sgovoni/    -
+-- Notes:        --                                                    -
+------------------------------------------------------------------------
+
+USE [Alter_Column_DB];
+GO
+
+
+/*
+  1. Download sp_alter_column from https://github.com/segovoni/sp_alter_column
+
+  2. Execute sp-alter-column.sql in the database you have just installed
+     tSQLt framework (Alter_Column_DB for this example)
+*/
